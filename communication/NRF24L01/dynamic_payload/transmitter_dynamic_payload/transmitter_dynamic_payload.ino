c58ae03e973b874fab6b6a271e5fca959ecf931c
sandesharyal-mrcreative369 <aryalsandesh2082@gmail.com>
2026-06-26 16:21:10 +0530

    Added dynamic Payload system

---
+++ communication/NRF24L01/dynamic_payload/transmitter_dynamic_payload/transmitter_dynamic_payload.ino
@@ -0,0 +1,69 @@
+#include<SPI.h>
+#include<nRF24L01.h>
+#include<RF24.h>
+
+RF radio(4,5);  // CE and CSN
+
+//Address Pipe
+const byte address[5] = "00011";
+
+//Data to be sent
+char dataPacket[] = "Hello Receiver";
+
+//ACk to be received
+char ack[] = "";
+
+
+void setup() {
+ Serial.begin(115200);
+
+ if(!radio.begin()){
+  Serial.println("Hardware Error...");
+  while(1);
+ }
+
+ Serial.println("Started NRF comunnication..");
+
+ //Channel Set
+ radio.setChannel(100);
+
+ //Power Amplifier
+ radio.setPALevel(RF24_PA_LOW);
+
+ //Enable Auto ACK Payload
+ radio.enableAckPayload();
+
+//Enable Dynamic Payload
+radio.enableDynamicPayloads();
+
+  //Address Pipe
+ radio.openWritingPipe(address);
+
+ //Transmitter Mode
+ radio.stopListening();
+
+}
+
+void loop() {
+
+  bool success = radio.write(&dataPacket,sizeof(dataPacket));
+  Serial.print("Data-->receiver successfully: ");
+  Serial.println(dataPacket);
+
+  if(success){
+    Serial.println("Successfully Delievred");
+
+    if(radio.isAckPayloadAvailable()){
+      radio.read(&ack,sizeof(ack));
+      Serial.print("ACK Receive: ");
+      Serial.println(ack);
+
+    }
+
+  }
+  else{
+    Serial.println("Failed No ACK Received..");
+  }
+ 
+
+}
